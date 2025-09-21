4324bf2f120242effa170647ec4ff90aac1db74a
Christian Engel <engel.christian1994@gmail.com>
2025-09-21 11:22:52 +0200

    Nix setup

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Environment for my blog";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ruby_3_1
+            nodejs
+
+            gcc
+            gnumake
+            pkg-config
+          ];
+        };
+      });
+}
+
